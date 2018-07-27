c3e37012fe16376e0ca74c1f48154a98ec540937
giovanni <mormagli@gmail.com>
2018-07-27 13:38:13 -0500

    paern and quote checking done

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const Stack = require('./stack');
 const util = require('util');
 
@@ -12,13 +14,15 @@ function main(){
   // startreck.pop();
 
   // console.log(util.inspect(startreck, false, null));
-  peek(startreck);
-  remove(startreck, 'McCoy');
-  display(startreck);
-  console.log(is_palindrome('dad'));
-  console.log(is_palindrome('A man, a plan, a canal: Panama'));
-  console.log(is_palindrome('1001'));
-  console.log(is_palindrome('Tauhida'));
+  // peek(startreck);
+  // remove(startreck, 'McCoy');
+  // display(startreck);
+  // console.log(is_palindrome('dad'));
+  // console.log(is_palindrome('A man, a plan, a canal: Panama'));
+  // console.log(is_palindrome('1001'));
+  // console.log(is_palindrome('Tauhida'));
+
+  console.log(matchingPar(`("({})")`)); 
 
 }
 
@@ -68,5 +72,45 @@ function is_palindrome(str) {
     }
   }
   return true;
+}
+
+
+function matchingPar(str){
+
+  let stack = new Stack();
+
+  for (let i = 0; i < str.length; i++) {
+    // console.log(stack.top)
+    
+    let topParen;
+    try {
+      topParen = stack.top.element.paren;
+    } catch (error) {}
+
+    if(topParen === '\'' || topParen === '"' ){
+      // console.log('here we are')
+      if(stack.top.element.paren === str[i]){
+        stack.pop();
+      }
+    }
+    
+    // if((stack.top.paren === '\'' && str[i] !== '\'') || (stack.top.paren === '"' && str[i] !== '"')){}
+
+    else if(str[i] === '(' || str[i] === '[' || str[i] === '{' || str[i] === '\'' || str[i] === '"'){
+      stack.push({paren: str[i], index: i});
+    }
+    else{
+
+      if(stack.top === null){return new Error(`extra close paren at index ${i}`);}
+      else{
+        let top = stack.pop();
+        if( str[i] === ')' && top.paren !== '('){ return new Error(`closing statement at ${i} doesn't match`);}
+        else if( str[i] === ']' && top.paren !== '['){ return new Error(`closing statement at ${i} doesn't match`);}
+        else if( str[i] === '}' && top.paren !== '{'){ return new Error(`closing statement at ${i} doesn't match`);}
+      }
+    }
+  }
+  
+  return stack.top === null? true : new Error(`extra open paren at index ${stack.top.element.index}`);
 }
 
